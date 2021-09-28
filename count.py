0fedccae06df845a4983e927e43b0be6146c28ac
L0LXD <91483769+L0LXD@users.noreply.github.com>
2021-09-28 22:56:54 +0530

    done ig

--- count.py
+++ count.py
@@ -1,4 +1,12 @@
 import numpy as np
+def grouping(lower,upper,class_interval,inc=False):    
+    lower_bd = np.arange(lower,upper,class_interval)
+    upper_bd = np.arange(lower_bd[1],upper+1,class_interval)
+    if inc ==True: 
+        lower_bd = (lower_bd - 1)
+        lower_bd[0]+=1
+    mid_pts = (upper_bd + lower_bd)/2
+    return lower_bd,upper_bd,mid_pts
 def count(X,grouped=False):
     info = {}
     if grouped== False:
