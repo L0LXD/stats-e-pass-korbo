cc9df5d9518bb54ac028950a6b04a65e30f468fc
L0LXD <91483769+L0LXD@users.noreply.github.com>
2021-09-28 12:27:38 +0530

    idk

--- median.py
+++ median.py
@@ -3,15 +3,15 @@ import numpy as np
 #ungrouped
 def median(X):
     return np.median(X)
-#grouped
 def median_freq(X,freqs):
-    data = []
+    temp = []
+    itr = 0
     for freq in freqs:
-        data =[X[temp]for i in freq]
-        temp+=1
-    median = np.median_grouped(data)
+        temp+=([data[itr]for i in range(freq)])
+        itr+=1
+    data = np.array(temp)
     cf = [0]
     for a in freq:
         cf.append(a+cf[-1])
     cf.pop(0)
-        
+    median = np.median_grouped(data)
