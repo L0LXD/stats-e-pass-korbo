ef47ff429c8d3d619fa1ee7a57648ba262c24c69
L0LXD <91483769+L0LXD@users.noreply.github.com>
2021-09-30 12:50:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-import numpy as np
-import mean as m
-from median import *
-X = np.array([47.5,52.5,57.5,62.5,67.5,72.5,77.5])
-freq = np.array([5,8,30,25,14,12,6])
-A = np.median(X)
-'''sed = m.range_mean(X,freq,A)
-print(sed['A'],sed['D'],sed['freq'],sed['FD'])
-print(sed['mean'])
-m.step_deviation(X,freq,62.5)'''
-X =  np.array([25,20,15,35,8])
-print(median_freq(2,X))
+class data:
+    def __init__(self,X,operation,freq=[],grped=True,):
+        grped = self.grped
+        operation = self.operation
+        freq = self.freq
+        if grped==False:
+            X = self.X
+        if grped==True:
+            (upper,lower,class_intervel)= self.X
+            if freq ==[]:
+                print('input frequencies or it wouldnt work')
